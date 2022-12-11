6f3561c36159a6f55709ab18d8b38f5ec6a72302
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2022-12-11 21:02:48 +0300

    define function with parameters and keyword parameters

---
+++ Day8/day8-1.py
@@ -0,0 +1,16 @@
+
+def greet():
+    for i in range(3):
+        print(f"Hello {i}")
+        
+
+greet()
+
+def greeting(name="Camilla",message="Hello"):
+    print(f"Hello {name} {message}")
+    
+
+greeting()
+
+greeting(message="Esselam",name="Uyghur")
+    
