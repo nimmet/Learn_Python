c4761a5512062ac50ffa7e1d89d56f0760a5e57d
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2022-12-15 22:15:54 +0300

    added encryption functionality

--- Day8/caesar-cipher.py
+++ Day8/caesar-cipher.py
@@ -1,26 +1,43 @@
 import string
 
-alpha = list(string.ascii_lowercase)
-cipher = list(alpha[9:]+alpha[0:9])
+
 
 str = list("hello world!")
 
 test = ['h','e','l','l','o']
 # ,'w','o','r','l','d','z'
-shift = 9
+
 encode = []
 
-print(str)
-for j in str: 
-    for i,v in enumerate(alpha):
-        if j == v:
-            encode.append(cipher[i])
-            print(cipher[i],end='')
+print("Welcome to Caesar Cipher!")
+stopEncode = False
+
+
+
+while not stopEncode:
+    coding = input("Type \'encode\' to encrypt, type \'decode\' to decrypt: ")
+    message = input("Type your message: ")
+    shift = int(input("Type the shift number: "))
+    alpha = list(string.ascii_lowercase)
+    cipher = list(alpha[shift:]+alpha[0:shift])
+    
+    
+    for j in list(message): 
         
-        
+        for i,v in enumerate(alpha):
+            if j == v:
+                encode.append(cipher[i])
             
-    if j == ' ':
-        encode.append(' ')
+                   
+        if not j.isalpha():
+            encode.append(j)
+    
+    print(f'Here is the encoded result: {"".join(encode)}')
+    again = input("Type \'yes\' if you want to go again. Otherwise type \'no\':")
+    if  again == 'no':
+        stopEncode=True
+        
+    
            
     
            
@@ -28,6 +45,6 @@ for j in str:
             
             
 
-print(encode)
-print(string.ascii_lowercase)
-print(string.ascii_lowercase[9:]+string.ascii_lowercase[0:9])
+
+# print(string.ascii_lowercase)
+# print(string.ascii_lowercase[9:]+string.ascii_lowercase[0:9])
