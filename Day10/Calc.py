59d2536d4fb9bb23bb279b4231de89724ed7238b
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2022-12-20 21:06:39 +0300

    calculator updated

--- Day10/Calc.py
+++ Day10/Calc.py
@@ -1,8 +1,8 @@
 quit =""
 result=0
-again=""
 
-while quit !="q":
+
+def calculate():
     first = int(input("What is the first number?: "))
     for i in ["+","-","*","/"]:
         print(i)
@@ -22,28 +22,53 @@ while quit !="q":
     else:
         result=round(first/second,2)
         print(f"{first} {operator} {second} = {result}")
+    return result
+
+def calculateWithResult(result):
     
-    if again == "y":
-        first = int(input("What is the first number?: "))
-        for i in ["+","-","*","/"]:
+    for i in ["+","-","*","/"]:
             print(i)
         
-        operator=input("Pick an operator: ")
-        second = int(input("What is the second number?: "))
-        
-        if operator == "+":
-            result=first+second
-            print(f"{first} {operator} {second} = {result}")
-        elif operator == "-":
-            result=first-second
-            print(f"{first} {operator} {second} = {result}")
-        elif operator == "*":
-            result=first*second
-            print(f"{first} {operator} {second} = {result}")
-        else:
-            result=round(first/second,2)
-            print(f"{first} {operator} {second} = {result}")
+    operator=input("Pick an operator: ")
+    second = int(input("What is the second number?: "))
+    
+    if operator == "+":
+        result=result+second
+        print(f"{result} {operator} {second} = {result}")
+    elif operator == "-":
+        result=result-second
+        print(f"{result} {operator} {second} = {result}")
+    elif operator == "*":
+        result=result*second
+        print(f"{result} {operator} {second} = {result}")
+    else:
+        result=round(result/second,2)
+        print(f"{result} {operator} {second} = {result}")
+    return result
+    
+    
+
+while quit !="q":
+    result=calculate() 
+    again = input("Do you want to continue with this result? y,n or q for quit: ")
+
+    
+    
+    
+    
+    if again == "y":
+        # first = int(input("What is the first number?: "))
+        result = calculateWithResult(result)
             
+        again = input("Do you want to continue with this result? y,n or q for quit: ")
+    
+    elif again=='n':
+        result=calculate()
+        again = input("Do you want to continue with this result? y,n or q for quit: ") 
+        result = calculateWithResult(result)
+    else:
+        print("Thank very much for using this calculator!")
+        quit="q"       
     
 
 
