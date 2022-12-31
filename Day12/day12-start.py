0303a947c49acf5abfbe1b39307a09b7edbf193d
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2022-12-31 21:58:29 +0300

    global and local scope. we can use return statement to upgrade our variable

--- Day12/day12-start.py
+++ Day12/day12-start.py
@@ -7,3 +7,12 @@ def increment_enemies():
     
 increment_enemies()
 print(f"enemies outside function: {enemies}")
+
+def increment_enemies(enemies):
+    enemies += 2
+    print(f"enemies inside function with parameter: {enemies}")
+    return enemies
+
+enemies = increment_enemies(enemies)
+print(f"enemies outside function with parameter: {enemies}")
+    
