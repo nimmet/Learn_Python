0fb096c71d307bc219dab1dff44a9c2902b59a6e
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-01-15 21:04:19 +0300

    added Todo app

---
+++ Day2/todo.py
@@ -0,0 +1,16 @@
+
+todo =""
+quit=""
+count=0
+todos=[]
+
+while quit != "y":
+    todo = input("Enter a todo: ")
+    if todo !="y": 
+        todos.append(todo)
+    else:
+        quit ="y"
+    
+    for i in range(len(todos)):
+        print(f"{i+1} - {todos[i]}")
+    
