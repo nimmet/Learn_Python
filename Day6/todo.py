92e554745ce1c67cc074c2f6e1a18368825691c1
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-01-21 21:32:37 +0300

    get todo list from file and store todo to file

--- Day6/todo.py
+++ Day6/todo.py
@@ -1,39 +1,43 @@
 import os
-todos = []
+# todos = []
 
 while True:
     
-    action = input("Type add, show, edit, clear, file, complete or exit: ").strip()
+    action = input("Type add, show, edit, clear, complete or exit: ").strip()
     
     match action:
         
-        case "file":
-            todo = input("Enter a todo from a file: ")+"\n"
-            file = open('todo.txt', 'r')
-            todo = file.readline()
-            file.close()
-            todos.append(todo.title())
-            
-            file = open('todo.txt', 'a')
-            file.writelines("\n"+todo)
-            file.close()
+        # case "file":
+        #     todo = input("Enter a todo from a file: ")
+        #     file = open('todo.txt', 'r')
+        #     todo = file.readline()
+        #     file.close()
+        #     todos.append(todo.title())
             
             
         case "add":
-            todo = input("Enter a todo: ")
-            todos.append(todo.title())
+            todo = input("Enter a todo: ")+"\n"
+            # todos.append(todo.title())
             file = open('todo.txt', 'a')
-            file.writelines("\n"+todo)
+            file.writelines(todo.title())
             
         case "show" | "display":
             file = open('todo.txt', 'r')
             todos = file.readlines()
             for i in range(len(todos)):
                 print(f"{i+1} - {todos[i]}")
+            file.close()
+            
         case "edit":
+            file = open('todo.txt', 'r')
+            todos = file.readlines()
+            file.close()
             num = int(input("Number of the todo to edit:"))
             new_todo = input("Enter new todo: ")
             todos[num-1]=new_todo.title()
+            file = open('todo.txt','w')
+            file.writelines(todos)
+            file.close()
             
         case "complete":
             num = int(input("Number of the todo to complete: "))
@@ -47,5 +51,6 @@ while True:
         
         case _:
             print("Please just enter the command offered")
+print("Bye!")
         
     
