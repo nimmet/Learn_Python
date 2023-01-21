ef50559b8d6aa3b67a104d3cc22accdb48225d05
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-01-21 22:13:56 +0300

    used zip method to create files and contents as we did just before

--- Day6/bonus.py
+++ Day6/bonus.py
@@ -1,8 +1,12 @@
 contents = ["I want to learn python","Do you think programming is so easy?","I hope i can work as SOC from Juni"]
 filenames=["doc.txt","report.txt","present.txt"]
 
-for index,content in enumerate(contents):
-    file=open(filenames[index],'w')
-    file.writelines(content)
+# for index,content in enumerate(contents):
+#     file=open(filenames[index],'w')
+#     file.writelines(content)
+#     file.close()
     
+for filename,content in zip(filenames,contents):
+    file = open(filename,"w")
+    file.write(content)
     
