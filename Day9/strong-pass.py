a0fd6bdddc7f79c3a9174758effe847d7d655c7f
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-01-26 22:05:51 +0300

    used dictionary

--- Day9/strong-pass.py
+++ Day9/strong-pass.py
@@ -1,13 +1,19 @@
 
-
+result ={}
 passwd = input("Enter new password: ")
 
 digit = [True for i in passwd if i.isdigit() ]
 digit= any(digit)
+result["digit"] = digit
+
 upper = [True for i in passwd if i.isupper()]
 upper = any(upper)
+result["upper"] = upper
+
 special = [True for i in passwd if i.isprintable()]
 special = any(special)
+result["special"] = special
+
 
 
 if len(passwd) >= 6 and all([digit,upper,special]):
@@ -17,5 +23,8 @@ elif len(passwd) >= 6 and digit and upper == False:
 elif len(passwd) >= 6:
     print("Password is ok, but not too strong")
     
+    
+if all(result.values()):
+    print("Strong password")
 else:
     print("Weak password")
