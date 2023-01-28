0bd5070c4f75f6a0aa532bd28aa2f5fa1cb61e64
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-01-28 21:54:12 +0300

    percentage calculator updated

--- Day10/code-exercise.py
+++ Day10/code-exercise.py
@@ -5,8 +5,14 @@ while True:
     try:
         total = int(input("Enter total value: "))
         val = int(input("Enter value: "))
+        
+        if not str(val).isdigit():
+            raise ValueError
 
-        print(f"That is {(val/total):.2f}%")
+        if total == 0:
+            print("You total value cannot be zero.")
+        else:
+            print(f"That is {(val*100/total):.2f}%")
         
         
     except ValueError:
