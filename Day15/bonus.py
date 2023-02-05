ebaa1d7aa10008ba141eca514d70d8f0bef46543
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-02-05 21:55:48 +0300

    updated

--- Day15/bonus.py
+++ Day15/bonus.py
@@ -1,9 +1,31 @@
 import json
 
+
+correct_answer=0
+incorrect_answer=0
+
+
 with open("quiz.json","r") as file:
     content = file.read()
     
 data = json.loads(content)
 
 for question in data:
-    print(question["question_text"])
+    print(question["question_text"].capitalize())
+    for index,option in enumerate(question["alternatives"]):
+        print(f"{index}-{option.capitalize()}")
+    user_input = int(input("Enter your answer: "))
+    if user_input == question["answer"]:
+        correct_answer += 1
+        
+        
+    else:
+        incorrect_answer += 1
+        
+        
+        
+
+
+
+    
+        
