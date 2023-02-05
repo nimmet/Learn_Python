b0f7742c3689678301703bf107ba798dea5fbcec
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-02-05 22:36:40 +0300

    quiz can do some statistics based on user input

--- Day15/bonus.py
+++ Day15/bonus.py
@@ -3,7 +3,8 @@ import json
 
 correct_answer=0
 incorrect_answer=0
-
+answer = {}
+q = 1
 
 with open("quiz.json","r") as file:
     content = file.read()
@@ -15,17 +16,29 @@ for question in data:
     for index,option in enumerate(question["alternatives"]):
         print(f"{index}-{option.capitalize()}")
     user_input = int(input("Enter your answer: "))
-    if user_input == question["answer"]:
+    an={}
+    an["user"] = user_input
+    an["correct"] = question["answer"]
+    answer[f"Q{q}"] = an 
+    q += 1
+        
+for k,v in answer.items():
+   
+    if v["user"] == v["correct"]:
+        user = v["user"]
+        cor = v["correct"]
+        print(f" {k} Correct Answer: User Answer: {user}, Correct Answer: {cor}")  
         correct_answer += 1
-        
-        
     else:
+        user = v["user"]
+        cor = v["correct"]
+        print(f" {k} Incorrect Answer: User Answer: {user}, Correct Answer: {cor}")  
         incorrect_answer += 1
         
+print(f"Score {correct_answer}/{correct_answer+incorrect_answer}")   
+
         
         
-
-
-
+    
     
         
