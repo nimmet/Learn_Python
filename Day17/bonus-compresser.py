2c8f4b12fd8c610d7b21ccff5f3cf9df61bb1fef
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-02-22 19:58:35 +0300

    gui file compressor completed

--- Day17/bonus-compresser.py
+++ Day17/bonus-compresser.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg 
+from zip_creator import make_archive
 
 s_file = sg.Text("Select files to compress:")
 s_input = sg.InputText()
@@ -7,15 +8,16 @@ d_input = sg.InputText()
 filechooser = sg.FilesBrowse("Choose",key="files")
 folderchooser = sg.FolderBrowse("Choose",key="folder")
 compress = sg.Button("Compress")
-
+info = sg.Text("",key="info")
 
 window = sg.Window("File Zipper",layout=[[s_file,s_input,filechooser],
                                          [d_file,d_input,folderchooser],
-                                         [compress]])
+                                         [compress,info]])
 
 while True:
     event,values=window.read()
-    print(values)
     filepaths = values["files"].split(";")
     folder = values["folder"]
+    make_archive(filepaths,folder)
+    window["info"].update(value="Compression completed!")
 window.close()
--- Day17/zip_creator.py
+++ Day17/zip_creator.py
@@ -6,7 +6,8 @@ def make_archive(filepath,dest_dir):
     dest_path = pathlib.Path(dest_dir,"compressed.zip")
     with zipfile.ZipFile(dest_path, 'w') as archive:
         for file in filepath:
-            archive.write(file)
+            file = pathlib.Path(file)
+            archive.write(file, arcname=file.name)
             
             
 
