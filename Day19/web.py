c586664de98c8c59588a6e940cbfd9036b9eb226
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-03-08 22:06:30 +0300

    added new item to todo app

--- Day19/web.py
+++ Day19/web.py
@@ -2,6 +2,13 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"]+"\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+    
+    
+
 st.title("My Todo App")
 st.subheader("This is sub header")
 st.write("Todo app")
@@ -13,4 +20,5 @@ st.checkbox("Prepare for Pentest Certification")
 for td in todos:
     st.checkbox(td)
     
-st.text_input(label="",placeholder="Add new todo...")
+st.text_input(label="",placeholder="Add new todo...",
+              on_change=add_todo,key="new_todo")
