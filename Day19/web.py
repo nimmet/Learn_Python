32523b98c0b8539c4bc100140004dd7f2017630a
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-03-08 21:48:54 +0300

    Todo app with streamlit

--- Day19/web.py
+++ Day19/web.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import functions
 
-
+todos = functions.get_todos()
 st.title("My Todo App")
+st.subheader("This is sub header")
+st.write("Todo app")
+
+
+st.checkbox("Learn Python")
+st.checkbox("Prepare for Pentest Certification")
+
+for td in todos:
+    st.checkbox(td)
+    
+st.text_input(label="",placeholder="Add new todo...")
