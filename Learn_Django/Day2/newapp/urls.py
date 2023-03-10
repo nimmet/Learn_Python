6685716188dbc616c260aa0ea0b3acf36f143530
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-03-10 22:09:41 +0300

    added some test page

--- Learn_Django/Day2/newapp/urls.py
+++ Learn_Django/Day2/newapp/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",)
+    path("",views.home),
+    path("test",views.form)
 
 ]
--- Learn_Django/Day2/newapp/views.py
+++ Learn_Django/Day2/newapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def verifyName(request):
+    name = input("What is your name: ")
+    return HttpResponse(name)
+
+def home(request):
+    return render(request,'index.html')
+
+def form(request):
+    return render(request,'test.html')
