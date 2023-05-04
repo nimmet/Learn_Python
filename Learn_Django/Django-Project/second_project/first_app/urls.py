87035f2b15463ba4787ca678c3a443257d8442d3
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-05-04 19:55:07 +0300

    path redirect

--- Learn_Django/Django-Project/second_project/first_app/urls.py
+++ Learn_Django/Django-Project/second_project/first_app/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:topic>/',views.news_view),
-    path('<int:num1>/<int:num2>',views.add_view),
+    path('<int:num_page>',views.num_page_view),
+    path('<str:topic>/',views.news_view,name='topic-page'),
+]
+
+    # path('<int:num1>/<int:num2>',views.add_view),
     
     # path('finance/',views.finance_view),
     # path('politics/',views.politics_view),
     
-]
--- Learn_Django/Django-Project/second_project/first_app/views.py
+++ Learn_Django/Django-Project/second_project/first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse,Http404, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -11,7 +12,24 @@ articles = {
 
 
 def news_view(request,topic):
-    return HttpResponse(articles[topic])
+    try:
+        result = articles[topic]
+        return HttpResponse(articles[topic])
+    except:
+        # result = 'No page for that topic!'
+        # return HttpResponseNotFound(result)
+        raise Http404("404 Generic error")
+    
+
+def num_page_view(request,num_page):
+    topics_list = list(articles.keys())
+    topic = topics_list[num_page]
+    
+    webpage = reverse('topic-page',args=[topic])
+    
+    return HttpResponseRedirect(webpage)
+    
+           
 
 def add_view(request,num1,num2):
     add_result = num1+num2
