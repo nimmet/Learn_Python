333eca162311029f6a51063624c0058295d0a04a
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-05-04 21:18:37 +0300

    Django templates

--- Learn_Django/Django-Project/second_project/first_app/views.py
+++ Learn_Django/Django-Project/second_project/first_app/views.py
@@ -4,37 +4,55 @@ from django.urls import reverse
 
 # Create your views here.
 
-articles = {
-    'sports': 'Sports Page',
-    'finance': 'Finance Page',
-    'politics': 'Politics Page'
-}
+
+def simple_view(request):
+    return render(request,'first_app/example.html') # .html file
 
 
-def news_view(request,topic):
-    try:
-        result = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        # result = 'No page for that topic!'
-        # return HttpResponseNotFound(result)
-        raise Http404("404 Generic error")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# articles = {
+#     'sports': 'Sports Page',
+#     'finance': 'Finance Page',
+#     'politics': 'Politics Page'
+# }
+
+
+# def news_view(request,topic):
+#     try:
+#         result = articles[topic]
+#         return HttpResponse(articles[topic])
+#     except:
+#         # result = 'No page for that topic!'
+#         # return HttpResponseNotFound(result)
+#         raise Http404("404 Generic error")
     
 
-def num_page_view(request,num_page):
-    topics_list = list(articles.keys())
-    topic = topics_list[num_page]
+# def num_page_view(request,num_page):
+#     topics_list = list(articles.keys())
+#     topic = topics_list[num_page]
     
-    webpage = reverse('topic-page',args=[topic])
+#     webpage = reverse('topic-page',args=[topic])
     
-    return HttpResponseRedirect(webpage)
+#     return HttpResponseRedirect(webpage)
     
            
 
-def add_view(request,num1,num2):
-    add_result = num1+num2
-    result = f"{num1} + {num2} = {add_result}"
-    return HttpResponse(str(result))
+# def add_view(request,num1,num2):
+#     add_result = num1+num2
+#     result = f"{num1} + {num2} = {add_result}"
+#     return HttpResponse(str(result))
 
 
 # def finance_view(request):
